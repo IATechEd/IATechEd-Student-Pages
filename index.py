ee44e4fe048941400cbd7ca8e205d4094bbb45ad
lumilovesyou <felis.araneae@gmail.com>
2026-08-12 13:36:24 -0500

    Basic Page Layout

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./assets", static_folder="./assets")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/")
+def index():
+    return "Working!"
+
+@app.route("/base")
+def base():
+    return render_template("./pages/base.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
