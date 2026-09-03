e9750a0cc0e445b7ef1793ac7b7eb7fb673e8385
lumilovesyou <felis.araneae@gmail.com>
2026-09-03 14:40:39 -0500

    Finished student page loading

--- index.py
+++ index.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
+from os import path
+import json
 
-app = Flask(__name__, template_folder="./assets", static_folder="./assets")
+app = Flask(__name__, template_folder="./assets", static_folder="./assets/documents/")
 
 @app.route("/")
 def index():
     return "Working!"
 
-@app.route("/base")
-def base():
-    return render_template("./pages/base.html")
+@app.route("/<string:student>")
+def studentPage(student):
+    filePath = f"./assets/users/{student}.json"
+    if path.exists(filePath):
+        with open(filePath, "r") as file:
+            jsonFile = json.loads(file.read())
+            file.close()
+        return render_template(
+                "./pages/student.html",
+                **jsonFile
+            )
+    return "hi"
 
 if __name__ == '__main__':
     app.run(debug=True)
